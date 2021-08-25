f4f4ede5ad7f545a56bc86bf55af7bd2bd003936
HarishB <harishb2@citiustech.com>
2021-08-25 17:55:02 +0530

    scheduling project added

---
+++ src/main/java/com/ct/scheduling/SchedulingMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ct.scheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingMicroserviceApplication.class, args);
+	}
+
+}
