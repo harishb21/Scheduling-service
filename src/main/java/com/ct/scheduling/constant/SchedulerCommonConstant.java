e60465765b76e1a4311380d8e6f019c7417d6769
HarishB <harishb2@citiustech.com>
2021-10-30 19:01:17 +0530

    constant

--- src/main/java/com/ct/scheduling/constant/SchedulerCommonConstant.java
+++ src/main/java/com/ct/scheduling/constant/SchedulerCommonConstant.java
@@ -1,16 +1,7 @@
 package com.ct.scheduling.constant;
 
-
-
 public final class SchedulerCommonConstant {
 
-	private SchedulerCommonConstant() {
-		
-	}
-	public static final String SUCCESS = "SUCCESS";
-	public static final String BAD_REQUEST = "BAD REQUEST";
-	public static final String NOT_FOUND = "NOT FOUND";
-	public static final String INTERNAL_SERVER_ERROR = "INTERNAL SERVER ERROR";
 	public static final String MAILSERVICEURL ="http://localhost:8082/mail/send/";
 	public static final String PATIENTSERVICE= "http://localhost:8082/patients/";
 	public static final String EMPLOYEEBYID = "http://localhost:8082/employees/employeeId/";
@@ -19,4 +10,7 @@ public final class SchedulerCommonConstant {
 	public static final String PATIENTERROR="Given PatientId is not found. please provide valid PatientId";
 	public static final String ROLESERVICE ="http://localhost:8082/roles/";
 	public static final String ROLEERROR = "Given Role Id not found. please provide valid RoleId";
+	private SchedulerCommonConstant() {
+		
+	}
 }
