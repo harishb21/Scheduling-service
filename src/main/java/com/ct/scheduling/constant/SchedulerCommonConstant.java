ce081e1256acf1fb6a3d4c6158518fabd44ea57a
Ketan pimpalshende <ketan.pimpalshende@citiustech.com>
2021-11-02 13:41:42 +0530

    bug resolved

--- src/main/java/com/ct/scheduling/constant/SchedulerCommonConstant.java
+++ src/main/java/com/ct/scheduling/constant/SchedulerCommonConstant.java
@@ -3,9 +3,9 @@ package com.ct.scheduling.constant;
 public final class SchedulerCommonConstant {
 
 	public static final String MAILSERVICEURL ="http://localhost:8082/mail/send/";
-	public static final String PATIENTSERVICE= "http://localhost:8082/patients/";
-	public static final String EMPLOYEEBYID = "http://localhost:8082/employees/employeeId/";
-	public static final String EMPLOYEES="http://localhost:8082/employees/";
+	public static final String PATIENTSERVICE= "http://localhost:8082/users/patients/";
+	public static final String EMPLOYEEBYID = "http://localhost:8082/users/employees/employeeId/";
+	public static final String EMPLOYEES="http://localhost:8082/users/employees/";
 	public static final String PHYSICIANERROR="Given PhysicianId is not found. please provide valid PhysicianId";
 	public static final String PATIENTERROR="Given PatientId is not found. please provide valid PatientId";
 	public static final String ROLESERVICE ="http://localhost:8082/roles/";
