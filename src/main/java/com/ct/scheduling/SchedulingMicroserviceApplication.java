b385599cd26ca0fe0172b40c56f18c2d216d3218
Ketan pimpalshende <ketan.pimpalshende@citiustech.com>
2021-11-02 13:11:21 +0530

    eureka client added final

--- src/main/java/com/ct/scheduling/SchedulingMicroserviceApplication.java
+++ src/main/java/com/ct/scheduling/SchedulingMicroserviceApplication.java
@@ -3,10 +3,11 @@ package com.ct.scheduling;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-//@EnableEurekaClient
+@EnableEurekaClient
 @SpringBootApplication
 public class SchedulingMicroserviceApplication {
 
